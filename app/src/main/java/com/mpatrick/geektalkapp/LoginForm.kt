5d98e7220a682dc19fd514910eaca3621ebec23a
MarcosPatrickExe <marcospatrick039474@gmail.com>
2022-10-10 00:14:18 -0300

    Criando Intent para interligar Activities da tela  de Login com a tela de Cadastro

--- app/src/main/java/com/mpatrick/geektalkapp/LoginForm.kt
+++ app/src/main/java/com/mpatrick/geektalkapp/LoginForm.kt
@@ -1,10 +1,14 @@
 package com.mpatrick.geektalkapp
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 
-class LoginForm : AppCompatActivity() {
+class LoginForm : AppCompatActivity( ) {
 
+        var  telaCadastroTxt: TextView  = findViewById(R.id.cria_conta_txt)
 
         override fun onCreate(savedInstanceState: Bundle?) {
                 super.onCreate(savedInstanceState);
@@ -12,5 +16,14 @@ class LoginForm : AppCompatActivity() {
 
                 // ESCONDENDO A TOOLBAR SUPERIOR:
                 super.getSupportActionBar()?.hide();
+
+
+               this.telaCadastroTxt.setOnClickListener{
+
+                       //   @Override fun onClick(view: View )  { }
+                        val intent : Intent = Intent( this,  SignUpForm::class.java )
+                        super.startActivity( intent );
+               }
         }
+
 }
