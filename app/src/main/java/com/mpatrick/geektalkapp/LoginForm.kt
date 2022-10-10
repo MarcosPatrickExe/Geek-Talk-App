8d46665f34e5b9e2686bc6c31ae220df1db967d9
MarcosPatrickExe <marcospatrick039474@gmail.com>
2022-10-09 22:19:45 -0300

    Tela de login concluída!!

--- app/src/main/java/com/mpatrick/geektalkapp/LoginForm.kt
+++ app/src/main/java/com/mpatrick/geektalkapp/LoginForm.kt
@@ -9,5 +9,8 @@ class LoginForm : AppCompatActivity() {
         override fun onCreate(savedInstanceState: Bundle?) {
                 super.onCreate(savedInstanceState);
                 setContentView(R.layout.activity_login_form);
+
+                // ESCONDENDO A TOOLBAR SUPERIOR:
+                super.getSupportActionBar()?.hide();
         }
 }
